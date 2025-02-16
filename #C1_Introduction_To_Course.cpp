31b9284c0ca65c86110a89a176ee961a7c62c677
Ajit Mandal <theajitmandal@gmail.com>
2025-02-16 16:41:46 +0000

    revised c++ course

--- #C1_Introduction_To_Course.cpp
+++ #C1_Introduction_To_Course.cpp
@@ -1,8 +1,9 @@
 // Copy to supreme 3.0
 
-//Chapter 1: Title ~ Introduction to Data Structure and AlgorithmsMaster Course (Supreme 3.0 Batch)
+//Chapter 1: Title ~ Introduction to Data Structure and Algorithms Master Course (Supreme 3.0 Batch)
 
 // 01. Welcome to Supreme 3.0 Batch
+    // Joining Discord Server
 // 02. Steps to Join Discord Group (Dashboard Introduction)
 // 03. Ticket Raised (if any issue)
 
